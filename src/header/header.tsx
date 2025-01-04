488dd5f68a66a572aca2709b7798e70b9f24b308
Gajanand Sokhal <gajanandsokhal0707@gmail.com>
2025-01-04 21:58:01 +0530

    header footer update

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -14,14 +14,14 @@ const Header = () => {
     };
 
     return (
-        <div className="header absolute w-full z-50 text-white">
+        <div className="header absolute w-full z-50 text-dark md:text-white">
             <header className="">
                 <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                     <div className="flex lg:flex-1">
                         <a href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
                             {/* <img className="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="" /> */}
-                       <h4>logo</h4>
+                       <h4 className="text-white">logo</h4>
                         </a>
                     </div>
                     <div className="flex lg:hidden">
@@ -31,7 +31,7 @@ const Header = () => {
                             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
                         >
                             <span className="sr-only">Open main menu</span>
-                            <svg className="size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                            <svg className="size-6 text-white" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                             </svg>
                         </button>
